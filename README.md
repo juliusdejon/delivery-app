628bc469bafdc7bfe849730050b85ced78a24b34
Julius Dejon <juliusdejon@gmail.com>
2023-10-04 03:42:20 -0400

    Init boilerplate for full stack development project

---
+++ README.md
@@ -0,0 +1,21 @@
+This was made for MADS4012 class
+
+# Pre-requisite
+
+1. Create a .env file inside the root directory
+2. Place this code below:
+
+   MONGO_DB_URL='';
+
+3. Go to MongoDB Atlas, Get your connection string
+4. Then supply it to the MONGO_DB_URL
+
+# Installation
+
+`npm install`
+
+# Development
+
+`npx nodemon server.js` or `npm start`
+
+#
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
+
+const HTTP_PORT = process.env.HTTP_PORT || 8080;
+const app = express();
+
+/*
+  middlewares
+*/
+app.use(express.static("assets"));
+app.use(express.urlencoded({ extended: true }));
+app.engine(".hbs", exphbs.engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+/*
+  db connection
+*/
+mongoose.connect(process.env.MONGO_DB_URL);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Error connecting to database: "));
+db.once("open", () => {
+  console.log("Mongo DB connected successfully.");
+});
+
+/*
+  restaurant endpoints
+*/
+
+app.get("/", (req, res) => {
+  res.send("Restaurant Website");
+});
+
+/*
+  order endpoints
+*/
+
+/*
+  delivery endpoints
+*/
+
+const onHttpStart = () => {
+  console.log(`Express web server running on port: ${HTTP_PORT}`);
+  console.log(`Press CTRL+C to exit`);
+};
+
+app.listen(HTTP_PORT, onHttpStart);
