f1540b0969fd55fd31a0cabaa178778948fa7901
Julius Dejon <juliusdejon@gmail.com>
2023-10-05 22:21:09 -0400

    Added orders endpoint

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ app.set("view engine", ".hbs");
   db connection
 */
 
-const MONGO_DB_URL = "";
-mongoose.connect(MONGO_DB_URL);
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "Error connecting to database: "));
-db.once("open", () => {
-  console.log("Mongo DB connected successfully.");
-});
+// const MONGO_DB_URL = "";
+// mongoose.connect(MONGO_DB_URL);
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "Error connecting to database: "));
+// db.once("open", () => {
+//   console.log("Mongo DB connected successfully.");
+// });
 
 /*
   restaurant endpoints
@@ -33,9 +33,86 @@ app.get("/", (req, res) => {
   res.render("index", { layout: false });
 });
 
+const itemsMock = [
+  {
+    _id: 1,
+    name: "Product1",
+    image: "donut.jpeg",
+    description: "Lorem ipsum",
+    price: 100,
+  },
+  {
+    _id: 2,
+    name: "Product2",
+    image: "donut.jpeg",
+    description: "Lorem ipsum",
+    price: 100,
+  },
+  {
+    _id: 3,
+    name: "Product3",
+    image: "donut.jpeg",
+    description: "Lorem ipsum",
+    price: 100,
+  },
+  {
+    _id: 4,
+    name: "Product4",
+    image: "donut.jpeg",
+    description: "Lorem ipsum",
+    price: 100,
+  },
+];
+
 /*
   order endpoints
 */
+
+const ordersMockData = [
+  {
+    _id: 1,
+    customerName: "John Doe",
+    deliveryAddress: "45 Four Winds Dr. North York, Toronto, ON",
+    itemsOrdered: [1, 4],
+    orderDate: "2023-10-05",
+    orderStatus: "Received",
+    proofOfDelivery: "",
+    driverName: "",
+    driverLicensePlate: "",
+  },
+  {
+    _id: 2,
+    customerName: "Finch West",
+    deliveryAddress: "22 Alphabet Dr. North York, Toronto, ON",
+    itemsOrdered: [3, 5, 1],
+    orderDate: "2023-10-06",
+    orderStatus: "Received",
+    proofOfDelivery: "",
+    driverName: "",
+    driverLicensePlate: "",
+  },
+  {
+    _id: 3,
+    customerName: "Jane Doe",
+    deliveryAddress: "46 Four Winds Dr. North York, Toronto, ON",
+    itemsOrdered: [3],
+    orderDate: "2023-10-06",
+    orderStatus: "Received",
+    proofOfDelivery: "",
+    driverName: "",
+    driverLicensePlate: "",
+  },
+];
+
+app.get("/orders", (req, res) => {
+  res.render("orders", {
+    layout: false,
+    orders: ordersMockData,
+    totalOrders: ordersMockData.length,
+  });
+});
+
+app.get("/orders/:orderCode", (req, res) => {});
 
 /*
   delivery endpoints
